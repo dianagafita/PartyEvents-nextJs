fc48e2e8d5ad188cec1f449f7a1ae8cfe440a34e
dianagafita <gafita.diana12@gmail.com>
2023-11-29 17:48:17 +0200

    finished

--- src/pages/editevent/[eventId]/index.js
+++ src/pages/editevent/[eventId]/index.js
@@ -1,30 +1,25 @@
-import { useRouter } from "next/router"; // Ensure the correct import statement
-
+import { useRouter } from "next/router";
 import EditEventForm from "../../../../components/Events/EditEventForm";
+import { useEffect } from "react";
 
 function EditEventPage() {
   const router = useRouter();
-  const userRole = localStorage.getItem("role"); // Get the user role from local storage
+  useEffect(() => {
+    const userRole = localStorage.getItem("role");
+    if (userRole !== "admin") {
+      router.push("/unouth");
+    }
+  }, []);
 
-  // Perform role-based checks
-  if (userRole !== "admin") {
-    // Redirect the user to the unauthorized page if they don't have the necessary role
-    router.push("/unouth");
-    // return <UnPage />;
-  }
-
-  const { eventId } = router.query; // Retrieve the 'id' from the router query
+  const { eventId } = router.query;
   console.log("ev", eventId);
-  // Check if 'id' is present or not before rendering the component
   if (!eventId) {
-    return <div>Loading...</div>; // Or handle the scenario when 'id' is not available
+    return <div>Loading...</div>;
   }
-
   return (
     <div>
       <h1>Edit Event {eventId}</h1>
       <EditEventForm eventId={eventId} />
-      {/* Pass the event ID to the form component */}
     </div>
   );
 }
